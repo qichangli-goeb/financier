5b4a6b6dd11e8d3e6e31c58c48146b158b90963d
qichangli <islagoeb@gmail.com>
2025-10-06 09:37:10 +0200

    Set up project with initializr

---
+++ src/test/java/com/isla/financier/FinancierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isla.financier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
