2a3d84fe765676db699e13e6b646cd9fd0d0149f
qichangli <islagoeb@gmail.com>
2025-10-06 14:32:04 +0200

    Implement Hello function.

---
+++ src/main/java/com/isla/financier/HelloController.java
@@ -0,0 +1,24 @@
+package com.isla.financier;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/everyone")
+    public @ResponseBody String helloEveryone() {
+        return "Hello, Everyone!";
+    }
+
+    @GetMapping("/{name}")
+    public @ResponseBody String helloByName(@PathVariable String name) {
+        return "Hello, " + name + ". How are you?";
+    }
+}
