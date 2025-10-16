1df924560a655a9377631199f1393c769d12fc74
qichangli <islagoeb@gmail.com>
2025-10-16 10:28:46 +0200

    delete transactions

--- frontend/src/components/transaction-item/TransactionItem.tsx
+++ frontend/src/components/transaction-item/TransactionItem.tsx
@@ -3,16 +3,23 @@ import type { Transaction } from "../../api/types.ts";
 
 export interface TransactionItemProps {
   transaction: Transaction;
+  onDelete: (id: number) => void;
 }
 
 function TransactionItem(props: TransactionItemProps) {
   return (
-    <>
-      <p className={styles.item}>
+    <div className={styles.item}>
+      <p>
         {props.transaction.id} / {props.transaction.description} /{" "}
         {props.transaction.amount}
       </p>
-    </>
+      <button
+        className={styles.delete}
+        onClick={() => props.onDelete(props.transaction.id)}
+      >
+        X
+      </button>
+    </div>
   );
 }
 export default TransactionItem;
--- frontend/src/components/transaction-list/TransactionList.tsx
+++ frontend/src/components/transaction-list/TransactionList.tsx
@@ -1,24 +1,34 @@
 import type { Transaction } from "../../api/types.ts";
 import TransactionItem from "../transaction-item/TransactionItem.tsx";
 import styles from "./TransactionList.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function TransactionList() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function getAllTransactions() {
+  async function loadTransactions() {
     const response = await fetch("http://localhost:5173/api/transactions");
     const tr: Transaction[] = await response.json();
     setTransactions(tr);
   }
 
+  async function deleteTransaction(id: number) {
+    await fetch(`http://localhost:5173/api/transactions/${id}`, {
+      method: "DELETE",
+    });
+    await loadTransactions();
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  });
+
   return (
     <div className={styles.list}>
       <h3>Transactions</h3>
       {transactions.map((item) => (
-        <TransactionItem transaction={item} />
+        <TransactionItem transaction={item} onDelete={deleteTransaction} />
       ))}
-      <button onClick={getAllTransactions}>Update</button>
     </div>
   );
 }
