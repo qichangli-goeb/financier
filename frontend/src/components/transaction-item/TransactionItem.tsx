2a71ee3fcf6589affdf5e85172f2eb3223fe5216
qichangli <islagoeb@gmail.com>
2025-10-24 23:23:53 +0200

    edit button

--- frontend/src/components/transaction-item/TransactionItem.tsx
+++ frontend/src/components/transaction-item/TransactionItem.tsx
@@ -1,6 +1,7 @@
 import styles from "./TransactionItem.module.css";
 import type { Transaction } from "../../api/types.ts";
-import { IconTrashOff } from "@tabler/icons-react";
+import { IconEdit, IconTrashOff } from "@tabler/icons-react";
+import { NavLink } from "react-router";
 
 export interface TransactionItemProps {
   transaction: Transaction;
@@ -14,9 +15,16 @@ function TransactionItem(props: TransactionItemProps) {
         {props.transaction.id} / {props.transaction.description} /{" "}
         {props.transaction.amount}
       </p>
-      <button onClick={() => props.onDelete(props.transaction.id)}>
-        <IconTrashOff color="orangered" />
-      </button>
+
+      <div className={styles.rightSide}>
+        <NavLink className="navButton" to={`edit/${props.transaction.id}`}>
+          <IconEdit />
+        </NavLink>
+
+        <button onClick={() => props.onDelete(props.transaction.id)}>
+          <IconTrashOff color="orangered" />
+        </button>
+      </div>
     </div>
   );
 }
